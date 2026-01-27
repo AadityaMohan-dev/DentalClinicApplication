70f0bee737fb462ba42856a1cc85a93ff1c430a4
AadityaMohan <aadimohan51@gmail.com>
2026-01-27 14:03:31 +0530

    inital commit

---
+++ src/test/java/com/clinic/DentalClinicApplication/DentalClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinic.DentalClinicApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
